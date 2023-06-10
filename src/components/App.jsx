45501eef6bc4e822878ef5bcab34301ecccc0911
mikhail-na <115450336+mikhail-na@users.noreply.github.com>
2023-06-10 18:23:25 +0200

    corrected

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,15 +16,15 @@ export const App = () => {
 
     switch (name) {
       case 'good':
-        setGood(good + 1);
+        setGood(good => good + 1);
         break;
       
       case 'neutral':
-        setNeutral(neutral + 1);
+        setNeutral(neutral => neutral + 1);
         break;
       
       case 'bad':
-        setBad(bad + 1);
+        setBad(bad => bad + 1);
         break;
       
       default:
